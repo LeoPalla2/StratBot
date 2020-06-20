2f74bed8923695d88d064d793f434e83e0138d91
PALLA Léo <palla.leo7@gmail.com>
2020-06-20 17:36:40 +0200

    version 2 of the bot

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python botv2.py
+worker: python bot.py
--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
 import discord
+import os
 from discord.ext.commands import Bot
 from discord.ext import commands
+from dotenv import load_dotenv
+from dotenv import find_dotenv
 
 
-
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+print(token)
 Client = discord.Client()
 bot_prefix = "!"
 client = commands.Bot(command_prefix=bot_prefix)
-bot_id= 382588984355717122
-channels_dictionary = {}
-tag = "[strat]"
+client.remove_command('help')
+guilds_dictionary = {}
+guilds_dictionary[565555865013321744] = {}
 @client.event
 async def on_ready():
     print("Bot Online")
@@ -17,44 +22,43 @@ async def on_ready():
     print("ID: {}".format(client.user.id))
 
 @client.event
+async def on_guild_join(guild):
+    if guild.id not in guilds_dictionary.keys():
+        guilds_dictionary[id] = {}
+
+@client.event
+async def on_guild_remove(guild):
+    if guild.id in guilds_dictionary.keys():
+        del guilds_dictionary[id]
+
+@client.event
 async def on_message(message):
-    await client.process_commands(message)
-    if tag in message.content and message.author.bot == False:
-        if message.channel.id in channels_dictionary.keys():
-            channel = client.get_channel(channels_dictionary[message.channel.id])
-            await channel.send(message.content+"\n"+"Original post: https://discordapp.com/channels/"+str(message.guild.id)+"/"+str(message.channel.id)+"/"+str(message.id))
-        else:
-            channel = message.channel
-            await channel.send("This channel isn't bound yet. use the bindChannel command to bind it")
+    ctx = await client.get_context(message)
+    if ctx.valid:
+        await client.process_commands(message)
+    else:
+        for (tag,channel) in guilds_dictionary[message.guild.id].items():
+            if tag in message.content and message.author.bot == False:
+                chan = client.get_channel(channel)
+                await chan.send(message.content+"\n"+"Original post: https://discordapp.com/channels/"+str(message.guild.id)+"/"+str(message.channel.id)+"/"+str(message.id))
+
         
 
 @client.command()
-async def bindChannel(ctx,channel: discord.TextChannel):
-    channels_dictionary[ctx.message.channel.id] = channel.id
-    await ctx.send("This channel is now bound to " + channel.mention)
-
-
-@client.command()
-async def setTag(ctx,newtag):
-    global tag
-    await ctx.send("Autodetection tag changed from " +tag + " to: " + newtag)
-    tag = newtag
+async def setTag(ctx,tag):
+    guilds_dictionary[ctx.guild.id][tag] = ctx.message.channel.id
+    await ctx.send("Messages containing \"" +tag + "\" will now be reposted in this channel")
 
 @client.command()
 async def showTag(ctx):
-    global tag
-    await ctx.send("Autodetection tag is: " +tag)
+    for (key,value) in guilds_dictionary[ctx.guild.id].items():
+        if value == ctx.message.channel.id:
+            await ctx.send("Auto detection tag for this channel is: " + key)
+            return
+    await ctx.send("No tag set for this channel")
 
-@setTag.error
-async def setTag_error(ctx,error):
-    if isinstance(error,commands.MissingRequiredArgument):
-        await ctx.send("Please specify a new autodetection tag")
+@client.command()
+async def help(ctx):
+    await ctx.send("```\nGeneral Use:\n\tThis bot is made to organise your strat channels. By setting an autodetection tag in a channel, the bot will repost any message containing the tag to this channel with a link to the original post. \n\nCommands: \n\tsetTag [tag]: Set the auto detection tag redirecting to the channel where the command is called\n\tshowTag: Show the auto detection tag currently set for the channel where the command is called\n```")
 
-@bindChannel.error
-async def bindChannel_error(ctx,error):
-    if isinstance(error,commands.BadArgument):
-        await ctx.send("This is not a valid channel on this server")
-    elif isinstance(error,commands.MissingRequiredArgument):
-        await ctx.send("Please specify the channel you want to link with")
-        
-client.run("MzgyNTg4OTg0MzU1NzE3MTIy.Xs_X4Q.kosXCT_6qABSG80IwEWrKvkR5VI")
+client.run(token)
